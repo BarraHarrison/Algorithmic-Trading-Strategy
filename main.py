5c1019438d92bbd373365360e651d5ed500d874d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-19 18:55:11 +0900

    Trying to visualize the moving averages

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # Algorithmic Trading Strategy in Python
-# pandas-datareader matplotlib
+import datetime as dt 
+import matplotlib.pyplot as plt 
+import pandas_datareader as web 
+
+plt.style.use("dark_background")
+
+moving_average_1 = 30
+moving_average_2 = 100
+
+start = dt.datetime.now() - dt.timedelta(days=365 * 3)
+end = dt.datetime.now()
+
+data = web.DataReader("FB", "yahoo", start, end)
+data[f"SMA_{moving_average_1}"] = data["Adj Close"].rolling(window=moving_average_1).mean()
+data[f"SMA_{moving_average_2}"] = data["Adj Close"].rolling(window=moving_average_2).mean()
+
+data = data.iloc[moving_average_2:]
+plt.plot(data["Adj Close"], label="Share Price", color="lightgray")
+plt.plot(data[f"SMA_{moving_average_1}"], label=f"SMA_{moving_average_1}", color="orange")
+plt.plot(data[f"SMA_{moving_average_2}"], label=f"SMA_{moving_average_2}", color="purple")
+plt.legend(loc="upper left")
+plt.show()
