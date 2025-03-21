4bd5d6c5fc8f1675149b4dbcf2eb79da25bff868
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-21 16:51:47 +0900

    Looking at Exponential Moving Average (EMA) instead of Simple Moving Average (SMA)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Algorithmic Trading Strategy in Python
 import datetime as dt 
 import matplotlib.pyplot as plt 
-import yfinance as yf 
+import yfinance as yf
+import pandas as pd 
 
 plt.style.use("dark_background")
 
@@ -13,17 +14,21 @@ end = dt.datetime.now()
 
 data = yf.download("TSLA", start=start, end=end, auto_adjust=False)
 
+if isinstance(data.columns, pd.MultiIndex):
+      data.columns = data.columns.droplevel(1)
+
 print(data.columns)
 
 if "Adj Close" in data.columns:
-    data[f"SMA_{moving_average_1}"] = data["Adj Close"].rolling(window=moving_average_1).mean()
-    data[f"SMA_{moving_average_2}"] = data["Adj Close"].rolling(window=moving_average_2).mean()
+    data[f"EMA_{moving_average_1}"] = data["Adj Close"].ewm(span=moving_average_1, adjust=False).mean()
+    data[f"EMA_{moving_average_2}"] = data["Adj Close"].ewm(span=moving_average_2, adjust=False).mean()
 
     data = data.iloc[moving_average_2:]
+
     plt.figure(figsize=(12, 6))
     plt.plot(data["Adj Close"], label="Share Price", color="lightgray")
-    plt.plot(data[f"SMA_{moving_average_1}"], label=f"SMA_{moving_average_1}", color="orange")
-    plt.plot(data[f"SMA_{moving_average_2}"], label=f"SMA_{moving_average_2}", color="purple")
+    plt.plot(data[f"EMA_{moving_average_1}"], label=f"EMA_{moving_average_1}", color="orange")
+    plt.plot(data[f"EMA_{moving_average_2}"], label=f"EMA_{moving_average_2}", color="purple")
     plt.legend(loc="upper left")
     plt.show()
 else:
@@ -35,11 +40,11 @@ sell_signals = []
 trigger = 0
 
 for x in range(len(data)):
-    if data[f"SMA_{moving_average_1}"].iloc[x] > data[f"SMA_{moving_average_2}"].iloc[x] and trigger != 1:
+    if data[f"EMA_{moving_average_1}"].iloc[x] > data[f"EMA_{moving_average_2}"].iloc[x] and trigger != 1:
             buy_signals.append(data["Adj Close"].iloc[x])
             sell_signals.append(float("nan"))
             trigger = 1
-    elif data[f"SMA_{moving_average_1}"].iloc[x] < data[f"SMA_{moving_average_2}"].iloc[x] and trigger != -1:
+    elif data[f"EMA_{moving_average_1}"].iloc[x] < data[f"EMA_{moving_average_2}"].iloc[x] and trigger != -1:
             buy_signals.append(float("nan"))
             sell_signals.append(data["Adj Close"].iloc[x])
             trigger = -1
@@ -54,8 +59,8 @@ print(data)
 
 plt.figure(figsize=(12, 6))
 plt.plot(data["Adj Close"], label="Share Price", alpha=0.5)
-plt.plot(data[f"SMA_{moving_average_1}"], label=f"SMA_{moving_average_1}", color="orange", linestyle="--")
-plt.plot(data[f"SMA_{moving_average_2}"], label=f"SMA_{moving_average_2}", color="pink", linestyle="--")
+plt.plot(data[f"EMA_{moving_average_1}"], label=f"EMA_{moving_average_1}", color="orange", linestyle="--")
+plt.plot(data[f"EMA_{moving_average_2}"], label=f"EMA_{moving_average_2}", color="pink", linestyle="--")
 plt.scatter(data.index, data["Buy Signals"], label="Buy Signal", marker="^", color="lime", linewidths=3)
 plt.scatter(data.index, data["Sell Signals"], label="Sell Signal", marker="v", color="red", linewidths=3)
 plt.legend(loc="upper left")
